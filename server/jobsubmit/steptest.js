d5e2f4cb443cd9f67ecbf7ca011bcc554d85ca12
chitaranjan <chitabiswal4@gmail.com>
2021-06-07 13:31:04 +0530

    jobsubmit

--- server/jobsubmit/steptest.js
+++ server/jobsubmit/steptest.js
@@ -1,6 +1,5 @@
 const axios = require("axios");
 
-// { command :"starttest", "msid":"apple services" ,"resolution":"lion resolution',userId:"60b0b5d851703802e8339620"}
 axios
   .post("http://localhost:8080/mslist", {
     id: "apple services",
