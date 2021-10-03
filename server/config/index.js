bb09e569292d559985db40c6d3d4dde62b894022
chitaranjan <chitabiswal4@gmail.com>
2021-10-03 11:38:00 +0530

    truth server error fixed

--- server/config/index.js
+++ server/config/index.js
@@ -3,7 +3,6 @@ module.exports = {
   MONGO_URI: "mongodb://localhost:27017",
   // MONGO_URI:
   // "mongodb+srv://pronab:xxed@cluster0.borpn.mongodb.net/truthdb?retryWrites=true",
-  // "mongodb+srv://chitaranjan:titan@cluster0.ojcpu.mongodb.net/truthdb?retryWrites=true",
 
   MONGO_DB_NAME: "truth-server",
   JWT_SECRET: "JWT_SECRET",
